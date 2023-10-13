0ad93b584f2b5e486c0d107625a1c64a6cea0485
K SHRAVAN <86780079+dark-coder-144@users.noreply.github.com>
2023-10-13 19:54:34 +0530

    Created Invite People and Manage Server Components

--- cinemaden/app/(main)/(routes)/servers/[serverId]/page.tsx
+++ cinemaden/app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,6 +1,6 @@
 const ServerPage = () => {
     return (
-        <div>Server ID Page</div>
+        <div className="md:flex h-full w-60 z-20 flex-col fixed inset-y-0">Server ID Page</div>
     )
 }
 export default ServerPage;
